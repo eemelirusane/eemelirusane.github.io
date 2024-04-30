1e0bb1cc59d38e3f6123e9a17146a1d1c972453f
Eemeli <eemeli.rusanen2@gmail.com>
2024-04-30 13:27:32 +0300

    juu

--- tehtavat/jsrocket/javascript/rocket.js
+++ tehtavat/jsrocket/javascript/rocket.js
@@ -4,13 +4,15 @@ var countdownNumber = 10;
 var changeState = function (state) {
     document.body.className = 'body-state'+ state;
  clearInterval(timer);
+ countdownNumber = 10;
+ document.getElementById('countdown').innerHTML = countdownNumber;
 
     if (state == 2) {
         //document.getElementById('countdown')//
         timer = setInterval(function () {
+            countdownNumber = countdownNumber
+            -1;
             document.getElementById('countdown').innerHTML = countdownNumber;
-             countdownNumber = countdownNumber
-              -1;
               if (countdownNumber <= 0) {
                 changeState(3);
               }
@@ -19,6 +21,7 @@ var changeState = function (state) {
         var success = setTimeout(function()
     {
     var randomNumber = Math.round (Math.random()*10);
+    console.log('randomNumber:', randomNumber)
     if (randomNumber > 5) {
         changeState(4);
 
